0ba18216fb73eb3c6dd9fe33f0035757fa6b6136
ClinicaDolor <gestogas.e@gmail.com>
2025-06-17 09:02:10 -0600

    Home actualización

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.gestogas.gestoline"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
